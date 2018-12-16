7a0a90d04229cc2725fe1f850183137dd5c925a5
Aliaksandr Shliayeu <shlyaeu@gmail.com>
2018-12-16 18:39:16 +0300

    adde controllers

--- js/app.js
+++ js/app.js
@@ -1,13 +1,45 @@
 const budgetController = (() => {})();
-const UIController = (() => {})();
-const controller = ((budgetCtrl, UICtrl) => {
-  const ctrlAddItem = () => {
-    console.log(`clicked`);
+const UIController = (() => {
+  const domStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
   };
-  document.querySelector(".add_btn").addEventListener("click", ctrlAddItem);
-  document.addEventListener("keypress", e => {
-    if (e.keyCode === 13 || e.which === 13) {
-      ctrlAddItem();
+  return {
+    getInput: () => {
+      return {
+        type: document.querySelector(domStrings.inputType).value,
+        description: document.querySelector(domStrings.inputDescription).value,
+        value: document.querySelector(domStrings.inputValue).value
+      };
+    },
+    getDomStrings: () => {
+      return domStrings;
     }
-  });
+  };
+})();
+const controller = ((budgetCtrl, UICtrl) => {
+  const setupEventListeners = () => {
+    const DOM = UICtrl.getDomStrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+    document.addEventListener("keypress", e => {
+      if (e.keyCode === 13 || e.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+  const ctrlAddItem = () => {
+    const input = UICtrl.getInput();
+    console.log("​ctrlAddItem -> input", input);
+  };
+  return {
+    init: () => {
+      console.log(`app started`);
+      setupEventListeners();
+    }
+  };
 })(budgetController, UIController);
+
+controller.init();
